b754e45cfaccd84204bbf7968c7260a7689b4c05
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-29 21:08:29 +0800

    Initial commit

---
+++ app/signup/page.js
@@ -0,0 +1,94 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+
+const SignUpPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email || !password || !confirmPassword)
+      return setErrorMessage("Please fill in all required fields.");
+
+    if (password !== confirmPassword)
+      return setErrorMessage("Your passwords do not match.");
+
+    console.log({ email, password, confirmPassword });
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <div className="w-full max-w-md px-4 py-8 bg-white rounded-lg shadow-lg">
+        <h2 className="mb-4 text-2xl font-semibold text-gray-700">Sign Up</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2 text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Your email"
+              className="w-full px-3 py-2 text-gray-700 border rounded-md focus:outline-none focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block mb-2 text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Your password"
+              className="w-full px-3 py-2 text-gray-700 border rounded-md focus:outline-none focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="confirmPassword"
+              className="block mb-2 text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Confirm password"
+              className="w-full px-3 py-2 text-gray-700 border rounded-md focus:outline-none focus:border-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+        </form>
+        <p className="m-2 text-center text-red-500">{errorMessage}</p>
+        <p className="mt-4 text-center text-gray-700">
+          Already have an account?{" "}
+          <Link href="/login" className="text-blue-500 hover:text-blue-700">
+            Log in here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
