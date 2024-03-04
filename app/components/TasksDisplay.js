f26c738af88a0e975646c9b37c1591bcc205b78f
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-04 16:09:16 +0800

    Create TaskDisplay component

---
+++ app/components/TasksDisplay.js
@@ -0,0 +1,56 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL } from "@/app/constants/constants";
+
+function TasksDisplay() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const authorization = {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("user")),
+        },
+      };
+
+      const response = await axios.get(
+        `${API_URL}/api/v1/tasks`,
+        authorization
+      );
+      setTasks(response.data.data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-wrap gap-4 mx-1 lg:mx-80 sm:mx-10 bg-[rgb(299,299,299)] p-4 mt-4">
+        {tasks.map((task) => {
+          const { attributes } = task;
+          return (
+            <div
+              key={task.id}
+              className="p-4 border-2 bg-[rgb(20,20,20)] text-white lg:w-[20%] rounded-lg"
+            >
+              <h2 className="my-2 text-xl text-center text-red-600">
+                {attributes.title}
+              </h2>
+              <div className="overflow-y-auto scrollbar max-h-[90%]">
+                {attributes.description}
+              </div>
+              <div className="p-2 text-center">Click for more info</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default TasksDisplay;
--- app/pages/dashboard/page.js
+++ app/pages/dashboard/page.js
@@ -1,23 +1,16 @@
-import Link from "next/link";
 import CategorySlider from "@/app/components/CategorySlider";
 import Navbar from "@/app/components/Navbar";
+import TasksDisplay from "@/app/components/TasksDisplay";
 
 function Dashboard() {
   return (
     <div className="flex flex-col h-screen">
-      {/* <header>
-        <Link href="./dashboard">
-          <div>Logo</div>
-        </Link>
-        <menu>
-          <li>
-            <Link href="./categories">Categories</Link>
-          </li>
-        </menu>
-      </header> */}
       <Navbar></Navbar>
       <div className="bg=[#e2e2e2]">
         <CategorySlider></CategorySlider>
+      </div>
+      <div className="bg=[#e2e2e2]">
+        <TasksDisplay></TasksDisplay>
       </div>
     </div>
   );
