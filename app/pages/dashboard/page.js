d4cf060b62240699d6c5089ec1e4b21a61700281
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-04 11:32:53 +0800

    Manually apply authorization

--- app/pages/dashboard/page.js
+++ app/pages/dashboard/page.js
@@ -1,7 +1,26 @@
-import React from "react";
+import Link from "next/link";
+import CategorySlider from "@/app/components/CategorySlider";
+import Navbar from "@/app/components/Navbar";
 
 function Dashboard() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col h-screen">
+      {/* <header>
+        <Link href="./dashboard">
+          <div>Logo</div>
+        </Link>
+        <menu>
+          <li>
+            <Link href="./categories">Categories</Link>
+          </li>
+        </menu>
+      </header> */}
+      <Navbar></Navbar>
+      <div className="bg=[#e2e2e2]">
+        <CategorySlider></CategorySlider>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
