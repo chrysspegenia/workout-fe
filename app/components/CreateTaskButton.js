32be4d191a2d6f3cab271cd40125de0ee1963b2b
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-07 15:48:33 +0800

    Create CreateTaskButton component

---
+++ app/components/CreateTaskButton.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const CreateTaskButton = () => {
+  return (
+    <Link href="./dashboard">
+      <button className="relative px-6 py-2 text-white bg-[rgb(20,20,20)] rounded-md hover:text-red-500 mx-1">
+        Create a task
+      </button>
+    </Link>
+  );
+};
+
+export default CreateTaskButton;
