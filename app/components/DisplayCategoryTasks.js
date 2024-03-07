a736eb0d83f98f0317ddb3b3f5a94d21d12e8f4a
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-07 23:00:38 +0800

    Create complete status button

--- app/components/DisplayCategoryTasks.js
+++ app/components/DisplayCategoryTasks.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import CreateTaskButton from "./CreateTaskButton";
 
 const DisplayCategoryTasks = () => {
-  const { targetCategory } = useApp();
+  const { targetCategory, targetTask, setTargetTask } = useApp();
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -26,7 +26,53 @@ const DisplayCategoryTasks = () => {
         authorization
       );
       setTasks(response.data.data);
-      console.log(response.data.data);
+      // console.log(response.data.data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  const handleSetTargetTask = (task) => {
+    // console.log(task);
+    setTargetTask({
+      title: task.attributes.title,
+      description: task.attributes.description,
+      image_url: task.attributes.image_url,
+      repetitions: task.attributes.repetitions,
+      sets: task.attributes.sets,
+      complete: task.attributes.completed,
+      category_id: task.attributes.category_id,
+      task_id: task.id,
+    });
+  };
+
+  const handleTaskCompleted = async () => {
+    try {
+      const authorization = {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("key")),
+        },
+      };
+
+      const updateTaskCompletedData = {
+        title: targetTask.title,
+        description: targetTask.description,
+        image_url: targetTask.image_url,
+        repetitions: targetTask.repetitions,
+        sets: targetTask.sets,
+        complete: targetTask.complete,
+        category_id: targetTask.category_id,
+        id: targetTask.id,
+      };
+
+      const response = await axios.patch(
+        `${API_URL}/api/v1/tasks/${targetTask.task_id}/complete`,
+        updateTaskCompletedData,
+        authorization
+      );
+
+      fetchCategoryTasks();
+      // console.log(response.data.task);
     } catch (error) {
       console.error("Error fetching categories:", error);
     }
@@ -42,7 +88,7 @@ const DisplayCategoryTasks = () => {
         </div>
       )}
       {tasks.length > 0 && (
-        <div className="m-10 bg-[rgb(299,299,299)] p-2 mx-1 lg:mx-80 sm:mx-10">
+        <div className="m-10 py-5 bg-[rgb(299,299,299)] p-2 mx-1 lg:mx-80 sm:mx-10">
           <span className="ml-10">
             <CreateTaskButton></CreateTaskButton>
           </span>
@@ -52,8 +98,7 @@ const DisplayCategoryTasks = () => {
               return (
                 <div
                   key={task.id}
-                  className="p-4 border-2 bg-[rgb(20,20,20)] text-white rounded-lg hover:bg-[rgb(40,40,40)] hover:cursor-pointer max-w-[20%]"
-                  // onClick={() => fetchTargetCategory(task.id)}
+                  className="p-4 border-2 bg-[rgb(20,20,20)] text-white rounded-lg hover:bg-[rgb(40,40,40)] hover:cursor-pointer w-[20%]"
                 >
                   <h2 className="my-2 text-xl text-center text-red-600">
                     {attributes.title}
@@ -61,7 +106,15 @@ const DisplayCategoryTasks = () => {
                   <div className="overflow-y-auto scrollbar max-h-[90%]">
                     {attributes.description}
                   </div>
-                  <div className="p-2 text-center">Click for more info</div>
+                  <button
+                    className={`px-6 py-1 text-white rounded-md ${
+                      attributes.completed ? "bg-green-500" : "bg-yellow-500"
+                    }`}
+                    onMouseOver={() => handleSetTargetTask(task)}
+                    onClick={handleTaskCompleted}
+                  >
+                    {attributes.completed ? "Completed" : "In Progress"}
+                  </button>
                 </div>
               );
             })}
