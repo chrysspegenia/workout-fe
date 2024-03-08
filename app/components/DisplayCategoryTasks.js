9e1db182b28127b6f0dc2cec9eae433ed23dc01e
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-08 17:26:07 +0800

    Modify layout and add EdiTaskForm

--- app/components/DisplayCategoryTasks.js
+++ app/components/DisplayCategoryTasks.js
@@ -4,10 +4,14 @@ import { API_URL } from "../constants/constants";
 import { useApp } from "../context/context";
 import { useEffect, useState } from "react";
 import CreateTaskButton from "./CreateTaskButton";
+import DeleteTaskButton from "./DeleteTaskButton";
+import EditTaskForm from "./EditTaskForm";
 
 const DisplayCategoryTasks = () => {
-  const { targetCategory, targetTask, setTargetTask } = useApp();
+  const { targetCategory, setTargetTask } = useApp();
   const [tasks, setTasks] = useState([]);
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   useEffect(() => {
     fetchCategoryTasks();
@@ -32,21 +36,7 @@ const DisplayCategoryTasks = () => {
     }
   };
 
-  const handleSetTargetTask = (task) => {
-    // console.log(task);
-    setTargetTask({
-      title: task.attributes.title,
-      description: task.attributes.description,
-      image_url: task.attributes.image_url,
-      repetitions: task.attributes.repetitions,
-      sets: task.attributes.sets,
-      complete: task.attributes.completed,
-      category_id: task.attributes.category_id,
-      task_id: task.id,
-    });
-  };
-
-  const handleTaskCompleted = async () => {
+  const handleTaskCompleted = async (task) => {
     try {
       const authorization = {
         headers: {
@@ -55,18 +45,18 @@ const DisplayCategoryTasks = () => {
       };
 
       const updateTaskCompletedData = {
-        title: targetTask.title,
-        description: targetTask.description,
-        image_url: targetTask.image_url,
-        repetitions: targetTask.repetitions,
-        sets: targetTask.sets,
-        complete: targetTask.complete,
-        category_id: targetTask.category_id,
-        id: targetTask.id,
+        title: task.attributes.title,
+        description: task.attributes.description,
+        image_url: task.attributes.image_url,
+        repetitions: task.attributes.repetitions,
+        sets: task.attributes.sets,
+        complete: task.attributes.complete,
+        category_id: task.attributes.category_id,
+        id: task.id,
       };
 
       const response = await axios.patch(
-        `${API_URL}/api/v1/tasks/${targetTask.task_id}/complete`,
+        `${API_URL}/api/v1/tasks/${task.id}/complete`,
         updateTaskCompletedData,
         authorization
       );
@@ -78,45 +68,72 @@ const DisplayCategoryTasks = () => {
     }
   };
 
+  const handleDeleteTask = async () => {};
+
   return (
     <>
       {tasks.length == 0 && (
-        <div className="lg:p-28 m-10 text-2xl text-center border-2 bg-[rgb(299,299,299)] p-2">
+        <div className="lg:p-28 my-10 mx-auto text-2xl text-center border-2 bg-[rgb(299,299,299)] p-2 2xl:w-[60%]">
           This category has no tasks. Click this
           <CreateTaskButton></CreateTaskButton>
           button.
         </div>
       )}
       {tasks.length > 0 && (
-        <div className="m-10 py-5 bg-[rgb(299,299,299)] p-2 mx-1 lg:mx-80 sm:mx-10">
-          <span className="ml-10">
+        <div className="m-5 py-5 bg-[rgb(299,299,299)] p-2 max-w-[70%] mx-auto 2xl:w-[60%]">
+          <span className="ml-20">
             <CreateTaskButton></CreateTaskButton>
           </span>
           <div className="flex flex-wrap justify-center gap-4 p-4">
             {tasks.map((task) => {
               const { attributes } = task;
-              return (
-                <div
-                  key={task.id}
-                  className="p-4 border-2 bg-[rgb(20,20,20)] text-white rounded-lg hover:bg-[rgb(40,40,40)] hover:cursor-pointer w-[20%]"
-                >
-                  <h2 className="my-2 text-xl text-center text-red-600">
-                    {attributes.title}
-                  </h2>
-                  <div className="overflow-y-auto scrollbar max-h-[90%]">
-                    {attributes.description}
-                  </div>
-                  <button
-                    className={`px-6 py-1 text-white rounded-md ${
-                      attributes.completed ? "bg-green-500" : "bg-yellow-500"
-                    }`}
-                    onMouseOver={() => handleSetTargetTask(task)}
-                    onClick={handleTaskCompleted}
+              if (editingTaskId === task.id) {
+                return (
+                  <EditTaskForm
+                    key={task.id}
+                    task={task}
+                    setEditingTaskId={setEditingTaskId}
+                    newTaskTitle={newTaskTitle}
+                    setNewTaskTitle={setNewTaskTitle}
+                    fetchCategoryTasks={fetchCategoryTasks}
+                  ></EditTaskForm>
+                );
+              } else {
+                return (
+                  <div
+                    key={task.id}
+                    className="px-6 py-2 border-2 bg-[rgb(20,20,20)] text-white rounded-lg hover:bg-[rgb(40,40,40)] hover:cursor-pointer flex flex-wrap items-center justify-between w-[90%]"
                   >
-                    {attributes.completed ? "Completed" : "In Progress"}
-                  </button>
-                </div>
-              );
+                    <h2 className="my-2 text-xl text-center text-red-600 xl:w-[50%] lg:w-full">
+                      {attributes.title}
+                    </h2>
+                    {/* <div className="overflow-y-auto scrollbar max-h-[90%]">
+                    {attributes.description}
+                  </div> */}
+                    <button
+                      className={`px-6 py-1 text-white rounded-md ${
+                        attributes.completed ? "bg-green-500" : "bg-yellow-500"
+                      }`}
+                      onClick={() => handleTaskCompleted(task)}
+                    >
+                      {attributes.completed ? "Completed" : "In Progress"}
+                    </button>
+                    <button
+                      className="px-6 py-1 text-black rounded-md bg-[#e5e5e5] hover:bg-[#ffffff]"
+                      onClick={() => {
+                        setTargetTask({ title: attributes.title });
+                        setEditingTaskId(task.id);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <DeleteTaskButton
+                      taskId={task.id}
+                      fetchCategoryTasks={fetchCategoryTasks}
+                    ></DeleteTaskButton>
+                  </div>
+                );
+              }
             })}
           </div>
         </div>
