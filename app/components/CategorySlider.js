8e44fb1c55773348e1154d7885cb9bfe4ce765ac
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-08 20:58:25 +0800

    Create handleClickTarget function

--- app/components/CategorySlider.js
+++ app/components/CategorySlider.js
@@ -2,9 +2,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { API_URL } from "@/app/constants/constants";
+import { useApp } from "../context/context";
 
 function CategorySlider() {
+  const { setTargetCategory } = useApp();
   const [categories, setCategories] = useState([]);
+  const [editCategoryBgId, seteditCategoryBgId] = useState(null);
 
   useEffect(() => {
     fetchCategories();
@@ -28,6 +31,17 @@ function CategorySlider() {
     }
   };
 
+  const handleClickTarget = (category) => {
+    setTargetCategory({
+      title: category.attributes.title,
+      // description: category.attributes.description,
+      description: "",
+      category_id: category.id,
+    });
+
+    seteditCategoryBgId(category.id);
+  };
+
   return (
     <div className="flex gap-2 p-2 mx-1 mt-4 overflow-x-auto lg:mx-80 sm:mx-10 bg-[rgb(299,299,299)] scrollbar 2xl:justify-center">
       {categories.map((category) => {
@@ -35,7 +49,13 @@ function CategorySlider() {
         return (
           <div
             key={category.id}
-            className="px-2 py-1 text-center border-2 bg-[rgb(20,20,20)] text-white flex-shrink-0 hover:bg-red-600 hover:cursor-pointer"
+            className={`${
+              editCategoryBgId === category.id
+                ? "bg-red-600 hover:text-black"
+                : "bg-[rgb(20,20,20)] hover:text-red-600"
+            } px-4 py-1 text-center text-white flex-shrink-0 cursor-pointer
+            `}
+            onClick={() => handleClickTarget(category)}
           >
             <div className="my-2">{attributes.title}</div>
           </div>
