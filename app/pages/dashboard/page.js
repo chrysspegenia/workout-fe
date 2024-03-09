63d9ca8b63c3bbf05bf88e44fe8ee6c5e3df70f3
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-09 22:18:50 +0800

    Add DisplayDailyTask component

--- app/pages/dashboard/page.js
+++ app/pages/dashboard/page.js
@@ -1,16 +1,21 @@
+"use client";
 import CategorySlider from "@/app/components/CategorySlider";
 import DisplayCategoryTasks from "@/app/components/DisplayCategoryTasks";
+import DisplayDailyTasks from "@/app/components/DisplayDailyTasks";
 import Navbar from "@/app/components/Navbar";
+import { useState } from "react";
 
 function Dashboard() {
+  const [showDailyTasks, setShowDailyTasks] = useState(true);
   return (
     <div className="flex flex-col h-screen">
       <Navbar></Navbar>
-      <div className="bg-[#e2e2e2]">
-        <CategorySlider></CategorySlider>
+      <div>
+        <CategorySlider setShowDailyTasks={setShowDailyTasks}></CategorySlider>
       </div>
-      <div className="bg-[#e2e2e2]">
-        <DisplayCategoryTasks></DisplayCategoryTasks>
+      <div>
+        {showDailyTasks && <DisplayDailyTasks></DisplayDailyTasks>}
+        {!showDailyTasks && <DisplayCategoryTasks></DisplayCategoryTasks>}
       </div>
     </div>
   );
