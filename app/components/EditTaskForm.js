43bf2e4bd03aafbf9bc0802888119ff48eb9dfec
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-10 23:25:12 +0800

    Add input date

--- app/components/EditTaskForm.js
+++ app/components/EditTaskForm.js
@@ -11,6 +11,8 @@ const EditTaskForm = (props) => {
     setNewTaskTitle,
     fetchCategoryTasks,
     fetchDailyTasks,
+    newTaskDeadline,
+    setNewTaskDeadline,
   } = props;
   const { targetTask, setTargetTask, targetCategory, isDailyPage } = useApp();
 
@@ -40,6 +42,7 @@ const EditTaskForm = (props) => {
         sets: "",
         completed: task.attributes.completed,
         task_id: "",
+        due_date: newTaskDeadline,
       };
 
       const response = await axios.patch(
@@ -68,6 +71,13 @@ const EditTaskForm = (props) => {
         onChange={(e) => setNewTaskTitle(e.target.value)}
         className="w-1/2 px-3 py-2 text-black rounded-md focus:outline-none"
         required
+      ></input>
+
+      <input
+        type="date"
+        value={newTaskDeadline}
+        onChange={(e) => setNewTaskDeadline(e.target.value)}
+        className="px-3 py-2 text-black rounded-md focus:outline-none"
       ></input>
 
       <div className="flex gap-10">
