854fff9ea1f3b2529801d2cd4d033a1f25ed3d41
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-11 15:59:51 +0800

    Modify API_URL constant to direct to production url

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://127.0.0.1:3001";
+// export const API_URL = "http://127.0.0.1:3001"; //development url
+
+export const API_URL = "https://workout-api-development.onrender.com"; //production url set on render.com
