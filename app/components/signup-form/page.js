a77afa6647aa3afb4ec75a64c7e5d2de20010cea
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-12 11:26:27 +0800

    Include navbar

--- app/components/signup-form/page.js
+++ app/components/signup-form/page.js
@@ -11,6 +11,11 @@ const SignUpForm = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [isClick, setIsClick] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsClick(!isClick);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,8 +53,97 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-50">
-      <div className="w-full max-w-md px-4 py-8 bg-white rounded-lg shadow-lg">
+    <div className="flex flex-col h-screen bg-gray-50">
+      <nav className="bg-black">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center gap-10">
+              <div className="flex-shrink-0">
+                <Link
+                  href="../components/login-form"
+                  className="text-3xl text-red-600"
+                >
+                  LOGO
+                </Link>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="items-center ml-4 space-x-4">
+                <Link
+                  href="../components/login-form"
+                  className="p-2 text-white rounded-lg hover:bg-white hover:text-black"
+                >
+                  Login
+                </Link>
+                <Link
+                  href="../components/signup-form"
+                  className="p-2 text-white rounded-lg hover:bg-white hover:text-black"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+            <div className="flex items-center md:hidden">
+              <button
+                className="inline-flex items-center justify-center p-2 text-white rounded:md hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                onClick={toggleNavbar}
+              >
+                {isClick ? (
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="https://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6L12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="https://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        {isClick && (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <Link
+                href="../components/login-form"
+                className="block p-2 text-white rounded-lg hover:bg-white hover:text-black"
+              >
+                Login
+              </Link>
+              <Link
+                href="../components/signup-form"
+                className="block p-2 text-white rounded-lg hover:bg-white hover:text-black"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        )}
+      </nav>
+
+      <div className="self-center w-full max-w-md px-4 py-8 my-auto bg-white rounded-lg shadow-lg">
         <h2 className="mb-4 text-2xl font-semibold text-gray-700">Sign Up</h2>
         <form>
           <div className="mb-4">
--- app/page.js
+++ app/page.js
@@ -1,14 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
+import LoginForm from "./components/login-form/page";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        WORKOUT
-      </div>
-      <Link href="./components/login-form">Login</Link>
-      <Link href="./components/signup-form">Register</Link>
-    </main>
-  );
+  return <LoginForm></LoginForm>;
 }
